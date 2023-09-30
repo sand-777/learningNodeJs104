192dd5a09d8ab30f8786a3fbc352f0351089c031
Sandip <sandip.bagale34@gmail.com>
2023-09-30 19:50:36 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("hello world")
+
+console.log(1+2)
+const a = 10;
+var b = 20;
+b =40;
+// console.log(a)
+// console.log(b)
+const sum = a + b;
+// console.log(sum)
+
+//array
+const numbers = [10,20,30]
+// console.log(numbers[1]);
+
+// to calculate length of array
+
+// console.log(numbers.length)
+
+// objects
+const person = {
+ name: "a",
+ age: 21
+}
+// {key:value}
+console.log("This is " + person.name)
+console.log(person.age)
